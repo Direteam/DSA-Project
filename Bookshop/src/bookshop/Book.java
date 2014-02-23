69001f84f62fb4a1cf47180cf04fb0c7795a5605
DXWar <DXWar@DXWar-PC>
2014-02-23 23:37:52 +0530

    IBinaryNode with Book Class

--- Bookshop/src/bookshop/Book.java
+++ Bookshop/src/bookshop/Book.java
@@ -8,53 +8,53 @@ package bookshop;
 class Book implements IBinaryNode {
 
     //Book class Fields
-	private String bookTitle, authorName, authorSurname;
-	private int isbn;
+    private String bookTitle, authorName, authorSurname;
+    private int isbn;
 	
-        //Left Child and Right Child
-        Book leftChild, rightChild;
+    //Left Child and Right Child
+    Book leftChild, rightChild;
         
-	//Book class Properties
-	public String getBookTitle() {
+    //Book class Properties
+    public String getBookTitle() {
 		return bookTitle;
 	}
 	
-	public void setBookTitle(String bookTitle) {
+    public void setBookTitle(String bookTitle) {
 		this.bookTitle = bookTitle;
 	}
 
-	public String getAuthorName() {
+    public String getAuthorName() {
 		return authorName;
 	}
 
-	public void setAuthorName(String authorName) {
+    public void setAuthorName(String authorName) {
 		this.authorName = authorName;
-	}
+    }
 
-	public String getAuthorSurname() {
+    public String getAuthorSurname() {
 		return authorSurname;
-	}
+    }
 
-	public void setAuthorSurname(String authorSurname) {
+    public void setAuthorSurname(String authorSurname) {
 		this.authorSurname = authorSurname;
-	}
+    }
 
-	public int getIsbn() {
+    public int getIsbn() {
 		return isbn;
-	}
+    }
 
-	public void setIsbn(int isbn) {
+    public void setIsbn(int isbn) {
 		this.isbn = isbn;
-	}
+    }
 
-	//Constructor for the Book Class
-	public Book()
-	{
-		bookTitle = authorName = authorSurname = null;
-		isbn = 0;
-                leftChild = null;
-                rightChild = null;
-	}
+    //Constructor for the Book Class
+    public Book()
+    {
+	bookTitle = authorName = authorSurname = null;
+	isbn = 0;
+        leftChild = null;
+        rightChild = null;
+    }
         
     @Override
     public Book element() {
@@ -102,7 +102,6 @@ class Book implements IBinaryNode {
 
     @Override
     public boolean isRoot() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return false;
     }
-
 }
