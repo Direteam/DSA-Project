bc33baa09747e016f258f965e6ba74f25f00d2a2
DXWar <DXWar@DXWar-PC>
2014-02-25 23:56:37 +0530

    Connection Update - 1

--- Bookshop/src/bookshop/Connection.java
+++ Bookshop/src/bookshop/Connection.java
@@ -11,64 +11,43 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class Connection {
+public class Connection
+{
     ResultSet rs;
     Statement stm;  
     Connection connection;
     
-    void connect(JTable myTable) throws ClassNotFoundException, SQLException{
-            Class.forName("com.mysql.jdbc.Driver");
-        java.sql.Connection connection = DriverManager.getConnection(  
-                    "jdbc:mysql://localhost:3306/bookshop", "root", ""); 
+   public void connect(JTable myTable) throws ClassNotFoundException, SQLException
+   {
+        Class.forName("com.mysql.jdbc.Driver");
+        java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshop", "root", ""); 
         Statement stm=  connection.createStatement();
-         ResultSet resultSet = stm.executeQuery("SELECT * FROM book_details");
-           java.sql.ResultSetMetaData rstmt=  resultSet.getMetaData();
-          int Colums = rstmt.getColumnCount();
-          System.out.println(Colums);
+        ResultSet resultSet = stm.executeQuery("SELECT * FROM book_details");
+        java.sql.ResultSetMetaData rstmt=  resultSet.getMetaData();
+        int Colums = rstmt.getColumnCount();
+        System.out.println(Colums);
          DefaultTableModel dtb = new DefaultTableModel();
          Vector Colums_num = new Vector();
          Vector data_rows = new Vector();
-         
-         
          for(int i=1;i<(Colums+1);i++)
          {
-            
              Colums_num.addElement(rstmt.getColumnName(i));
-             
          }
-      
-          
          dtb.setColumnIdentifiers(Colums_num);
-         
-                
          while(resultSet.next())
          {
-               
              data_rows = new Vector();
                for(int j=1;(j<Colums+1);j++)
                {
-                    
                    data_rows.addElement(resultSet.getString(j));
-                    
                }
-                  
-                dtb.addRow(data_rows);
-                   
+                dtb.addRow(data_rows);         
          }
-            
-         
-         myTable.setModel(dtb);
-         
-          
-        
+         myTable.setModel(dtb);   
     }
-  
-        
-        
-   
 }
