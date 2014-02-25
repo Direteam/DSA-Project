1807894ac136561ba7a6c58d5f9ad8be1059d43b
DXWar <DXWar@DXWar-PC>
2014-02-25 23:49:38 +0530

    Book Class Update - 5

--- Bookshop/src/bookshop/BooksTree.java
+++ Bookshop/src/bookshop/BooksTree.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package bookshop;
-
-import javax.xml.soap.Node;
-
-/**
- *
+/*
  * @author himal
  */
 public class BooksTree
