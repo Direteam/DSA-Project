8e1a1a3a95cfb7e2b5e1d233f998beba81809fdf
Kapthura <Dirteam.github@gmail.com>
2014-02-26 00:56:13 +0530

    DB Connection class Update
    Due to  push fail

--- Bookshop/src/bookshop/DBConnection.java
+++ Bookshop/src/bookshop/DBConnection.java
@@ -3,6 +3,47 @@
  */
 package bookshop;
 
+ import java.sql.*;
+
 class DBConnection {
-    
+     java.sql.Connection connection;
+    Statement stm;
+
+    public void Connectdrivermanager() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error: unable to load driver class!");
+
+        }
+    }
+
+    public void InitiateDB() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshop", "root", "");
+        } catch (Exception ex) {
+            System.out.println("Error: DB Initiae failed!");
+        }
+
+    }
+
+    public void ConnectDB(String query) {
+        Connectdrivermanager();
+        InitiateDB();
+        try {
+            stm = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            stm.executeQuery(query);
+        } catch (Exception ex) {
+            System.out.println("Error: DB Connection failed!");
+        }
+    }
+
+    public void closedDB() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Error: DB Close failed!");
+        }
+
+    }
 }
